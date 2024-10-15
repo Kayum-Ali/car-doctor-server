10738b384cbd45d6d5e5308ee2905fcc0de5cdd1
Kayum <sisirfahimstudent173@gmail.com>
2024-10-15 23:32:33 +0600

    start server and update again

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require("express");
-const app = express();
 const jwt = require("jsonwebtoken");
-
-
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const cookieParser = require("cookie-parser");
-// middleware
 require("dotenv").config();
-app.use(cors({
-  origin: ['http://localhost:5173/'],
-  credentials : true
-}));
+const cookieParser = require("cookie-parser");
+
+const app = express();
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Car doctor server is Running");
+  res.send("Mechanic server is Running");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dangeag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,11 +32,29 @@ const client = new MongoClient(uri, {
   },
 });
 
+// const logger = async(req, res, next)=>{
+//   // console.log( req.hostname, req.originalUrl );
+//   next();
+// }
+
+// const verifyToken = (req, res, next) => {
+//   const token = req.cookies?.token;
+//   if (!token) {
+//     return res.status(401).send({ message: "Access denied No token provided" })
+//   }
+//    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if(err){
+//       return res.status(401).send({message: "unAuthorize"})
+//     }
+//     req.user = decoded;
+//     next();
+
+//    });
+   
+//   }
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
     const database = client.db("carDoctor");
     const servicesCollection = database.collection("services");
     const bookingsCollection = database.collection("bookings");
@@ -42,17 +62,26 @@ async function run() {
     // auth releted api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: "1h" }
-      );
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
       res
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'none'
-      })
-      .send({success: true})
+      .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({ success: true });
     });
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      res.clearCookie("token", {maxAge: 0}).send({ success: true });;
+    })
+
+
     // services collection
     app.get("/services", async (req, res) => {
       const services = await servicesCollection.find().toArray();
@@ -80,22 +109,31 @@ async function run() {
       res.send(result);
     });
 
-    // bookings
 
+    // bookings
     app.get("/bookings", async (req, res) => {
+      const user = req.body
+      // console.log("tok tok token", req.cookies?.token);
+      console.log('user',user)
+      // console.log( req?.query?.email);
+      if(req?.query?.email !== req.user?.email){
+        return res.status(401).send({ message: "Access denied No token provided" })
+      }
       let query = {};
       if (req.query?.email) {
-        query = { email: req.query.email };
+        query = { email: req?.query.email };
       }
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
     });
-    app.post("/bookings", async (req, res) => {
-      const newBooking = req.body;
 
+
+    app.post("/bookings",  async (req, res) => {
+      const newBooking = req.body;
       const result = await bookingsCollection.insertOne(newBooking);
       res.send(result);
     });
+
 
     app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
@@ -105,14 +143,14 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
+
+    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
-    // await client.close();
+   
   }
 }
 run().catch(console.dir);

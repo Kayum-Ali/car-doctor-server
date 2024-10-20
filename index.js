bcd431629b0c10445fec9b6f77980d0b48c20fd4
Kayum <sisirfahimstudent173@gmail.com>
2024-10-20 11:45:11 +0600

    when user logout then exe logout route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7001;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
@@ -36,26 +36,27 @@ const client = new MongoClient(uri, {
   },
 });
 
-const logger = async(req, res, next) => {
-  console.log(  req.originalUrl );
-  next();
+
+
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Access denied No token provided" })
+  }
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      
+      return res.status(401).send({message: "unAuthoerize"})
+    }
+  
+    req.user = decoded;
+    
+  
+    next();
+});
+
 }
-
-// const verifyToken = (req, res, next) => {
-//   const token = req.cookies?.token;
-//   if (!token) {
-//     return res.status(401).send({ message: "Access denied No token provided" })
-//   }
-//    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if(err){
-//       return res.status(401).send({message: "unAuthorize"})
-//     }
-//     req.user = decoded;
-//     next();
-
-//    });
-   
-//   }
 async function run() {
   try {
     await client.connect();
@@ -64,25 +65,27 @@ async function run() {
     const bookingsCollection = database.collection("bookings");
 
     //auth related api
-    app.post("/jwt", logger , async(req, res)=>{
+    app.post("/jwt" , async (req, res)=>{
       const user = req.body;
-      console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "365d",
+        expiresIn: "1h",
       });
 
-      res
-      .cookie('Access_token', token, {
+      res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      })
-      .send({ success: true })
+        secure:process.env.NODE_ENV === 'production', //
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // 
+      }).send({ success: true })
+    })
+
+    app.post("/logout",  (req, res)=>{
+     
+      res.clearCookie('token',{maxAge: 0}).send({ success: true })
     })
 
 
     // services collection
-    app.get("/services", logger, async (req, res) => {
+    app.get("/services", async (req, res) => {
       const services = await servicesCollection.find().toArray();
       res.send(services);
     });
@@ -93,7 +96,7 @@ async function run() {
       const result = await servicesCollection.findOne(query);
       res.send(result);
     });
-    app.get("/checkout/:id",logger, async (req, res) => {
+    app.get("/checkout/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
@@ -110,14 +113,12 @@ async function run() {
 
 
     // bookings
-    app.get("/bookings", async (req, res) => {
-      // const user = req.body
-      console.log("tok tok token", req.cookies?.Access_token);
-      // console.log('user',user)
-      // console.log( req?.query?.email);
-      // if(req?.query?.email !== req.user?.email){
-      //   return res.status(401).send({ message: "Access denied No token provided" })
-      // }
+    app.get("/bookings",verifyToken, async (req, res) => {
+    //  console.log(req?.query?.email)
+    //  console.log(req?.user?.email)
+      if(req?.query?.email !== req?.user?.email){
+        return res.status(401).send({ message: "Forbidden Access" })
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req?.query.email };

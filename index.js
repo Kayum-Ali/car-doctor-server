6fc564d14775437cbed070d40135a7aec41b442a
Kayum <sisirfahimstudent173@gmail.com>
2024-10-20 16:38:49 +0600

    BSON objID updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const port = process.env.PORT || 7001;
+const port = process.env.PORT || 50;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
@@ -9,6 +9,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const corsOptions = {
   origin: [
+    'http://localhost:5174',
     'genial-theory-434606-u4.web.app',
   ],
   credentials: true,
@@ -47,10 +48,7 @@ const verifyToken = (req, res, next) => {
       
       return res.status(401).send({message: "unAuthoerize"})
     }
-  
     req.user = decoded;
-    
-  
     next();
 });
 
@@ -66,18 +64,17 @@ async function run() {
     app.post("/jwt" , async (req, res)=>{
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "365d",
       });
 
       res.cookie('token', token, {
         httpOnly: true,
-        secure:process.env.NODE_ENV === 'production', //
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // 
+        secure:process.env.NODE_ENV === 'production', 
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
       }).send({ success: true })
     })
 
     app.post("/logout",  (req, res)=>{
-     
       res.clearCookie('token',{maxAge: 0}).send({ success: true })
     })
 
